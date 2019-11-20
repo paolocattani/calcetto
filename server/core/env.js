c052f5ca1a64aecabe5da2750faf1b056e907cab
paolocattani <paolocattani0501@gmail.com>
2019-11-20 12:05:59 +0100

    ADV - back to CRA
    provide a basic project structure

---
+++ server/core/env.js
@@ -0,0 +1,9 @@
+//https://github.com/motdotla/dotenv/blob/master/examples/typescript/src/lib/env.ts
+import { resolve } from 'path'
+import { config } from 'dotenv'
+
+config({
+    path: resolve(__dirname, "../../.env"),
+    debug: true,
+    silent: true
+})
--- server/model/server/AppServer.ts
+++ server/model/server/AppServer.ts
@@ -1,14 +1,18 @@
-import { AbstractServer } from "./AbstractServer";
-import { resolve } from 'path';
-import express, { Application as ExpressApplication, Request, Response } from 'express';
 import { logger } from "../../core/logger";
+import { AbstractServer } from "./AbstractServer";
+//import path from 'path';
+import express, { Application as ExpressApplication, Request, Response } from 'express';
+
 import * as util from 'util';
 
+const applicationName = 'ApplicationServer';
+const applicationPort = 8080;
+const applicationCPUs = 4;
 
 export class AppServer extends AbstractServer {
 
     constructor() {
-        super('ApplicationServer', 8080, 2);
+        super(applicationName, applicationPort, applicationCPUs);
         this.start();
     }
 
@@ -16,11 +20,6 @@ export class AppServer extends AbstractServer {
         application.get('/api', (req: Request, res: Response) =>
             res.status(200).send({ message: `Welcome to ${this.serverName} endpoint API!` })
         );
-
-        application.get('*', (req: Request, res: Response) => {
-            logger.info(`AppServer.routes : ${util.inspect(req.session)}`)
-            res.sendFile(resolve(__dirname, '../../build', 'index.html'));
-        });
     }
 
 }
