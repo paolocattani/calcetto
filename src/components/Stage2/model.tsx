1b1998b8325c448508bb3e01cd9405770522ff4f
paolocattani <paolocattani0501@gmail.com>
2020-02-17 08:19:15 +0100

    ADD - style to stage2

--- src/components/Stage2/model.tsx
+++ src/components/Stage2/model.tsx
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
-import { ListGroup } from 'react-bootstrap';
+import React, { useState, CSSProperties } from 'react';
+import { ListGroup, InputGroup, FormControl } from 'react-bootstrap';
 
 const Model: React.FC = _ => {
   const STAGE_NUMBER = 4;
+
+  const outerStyle: CSSProperties = {
+    flexDirection: 'row-reverse'
+  };
+
+  const innerStyle: CSSProperties = {
+    flexDirection: 'column-reverse'
+  };
 
   let element = [];
   for (let ii = 0; ii <= STAGE_NUMBER; ii++) {
@@ -10,19 +18,25 @@ const Model: React.FC = _ => {
     for (let jj = 0; jj < Math.pow(2, ii); jj++) {
       thisList.push(
         <ListGroup.Item>
-          {ii}-{jj}
+          <InputGroup>
+            <FormControl placeholder={`${ii}-${jj}`} />
+          </InputGroup>
         </ListGroup.Item>
       );
     }
     element.push(
       <ListGroup.Item>
-        <ListGroup variant="flush" id="inner-container">
+        <ListGroup variant="flush" id="inner-container" style={innerStyle}>
           {thisList}
         </ListGroup>
       </ListGroup.Item>
     );
   }
-  return <ListGroup id="outer-container">{element}</ListGroup>;
+  return (
+    <ListGroup id="outer-container" style={outerStyle}>
+      {element}
+    </ListGroup>
+  );
 };
 
 export default Model;
