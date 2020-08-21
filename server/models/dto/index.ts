15b93ec8c24141cc513865bcba408150370acd9b
paolocattani <paolocattani0501@gmail.com>
2020-08-21 13:40:59 +0200

    Fix import server

---
+++ server/models/dto/index.ts
@@ -0,0 +1,6 @@
+export * from './pair.dto';
+export * from './player.dto';
+export * from './stage1.dto';
+export * from './stage2.dto';
+export * from './tournament.dto';
+export * from './user.dto';
