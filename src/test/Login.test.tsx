884eb972e6202bedd11909de4efededcb2620d7d
paolocattani <paolocattani0501@gmail.com>
2020-09-01 18:14:58 +0200

    Testing

--- src/test/Login.test.tsx
+++ src/test/Login.test.tsx
@@ -2,6 +2,33 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Login from '../components/Auth/Login';
 import { shallowToJson } from 'enzyme-to-json';
+import configureStore from 'redux-mock-store';
+import renderer from 'react-test-renderer';
+
+import { initialState } from './commons';
+import { Provider } from 'react-redux';
+
+const mockStore = configureStore([]);
+describe('<Login />', () => {
+  let store;
+  let component: any;
+  beforeEach(() => {
+    store = mockStore(initialState);
+    component = renderer.create(
+      <Provider store={store}>
+        <Login />
+      </Provider>
+    );
+  });
+
+  it('Should render with initial store state', () => {
+    expect(shallowToJson(component)).toMatchSnapshot();
+  });
+
+  it('should dispatch an action on button click', () => {
+    expect(shallowToJson(component)).toMatchSnapshot();
+  });
+});
 
 test('<Login />', () => {
   const component = shallow(<Login />);
