e4af850031005e6f447ebbd7ff5e59c8cdea8b3f
paolocattani <paolocattani0501@gmail.com>
2020-09-02 13:28:42 +0200

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 echo '--> Gathering informations'
 export REACT_APP_CLIENT_COMMIT_HASH=$(git rev-parse --short HEAD)
 export REACT_APP_CLIENT_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
+
 echo    '------------------------'
-echo    '|  Commit  |   '$REACT_APP_CLIENT_COMMIT_HASH     ' | '
 echo    '|  Version |    '   $REACT_APP_CLIENT_VERSION        ' | '
+echo    '|  Commit  |   '$REACT_APP_CLIENT_COMMIT_HASH     ' | '
 echo    '------------------------'
+
+# https://zhu45.org/posts/2016/Dec/21/environment-variable-substitution-using-sed/
+sed -i 's|^REACT_APP_CLIENT_VERSION=.*$|REACT_APP_CLIENT_VERSION='$REACT_APP_CLIENT_VERSION'|g' .env
+sed -i 's|^REACT_APP_CLIENT_COMMIT_HASH=.*$|REACT_APP_CLIENT_COMMIT_HASH='$REACT_APP_CLIENT_COMMIT_HASH'|g' .env
 
 echo '--> Run Build'
 npm run CRA:build
-
-
-
-
