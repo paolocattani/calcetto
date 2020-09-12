ebfb46365f22a6401284c2a48a7245231c03e5c4
paolocattani <paolocattani0501@gmail.com>
2020-09-12 16:27:53 +0200

    Minors ( Test CI )

--- server/core/utils.ts
+++ server/core/utils.ts
@@ -10,7 +10,7 @@ export function getBaseLog(x: number, y: number) {
 // Async utils
 export async function asyncForEach<T>(
   array: Array<T>,
-  callback: (element: T, index: number, array: Array<T>) => void
+  callback: (element: T, index: number, array: Array<T>) => Promise<void>
 ): Promise<void> {
   for (let index = 0; index < array.length; index++) {
     await callback(array[index], index, array);
