637273d42bfc0b4925de111eb5699a2726db2dda
Paolo Cattani <pcattani@ebsconsultingsrl.it>
2020-09-23 08:42:48 +0200

    Swagger

--- src/components/Swagger/Swagger.tsx
+++ src/components/Swagger/Swagger.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import SwaggerUI from 'swagger-ui-react';
-import 'swagger-ui-react/swagger-ui.css';
-import definitions from './openapi.json';
+import definitions from './api/openapi.json';
+
 import './style.css';
+import 'swagger-ui-react/swagger-ui.css';
+
 // test : url="https://petstore.swagger.io/v2/swagger.json"
 export default () => <SwaggerUI spec={definitions} />;
