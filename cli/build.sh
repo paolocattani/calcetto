3b5610a9b14b822bd7461d66b90281047e46fc06
Paolo Cattani <paolocattani0501@gmail.com>
2020-10-27 09:37:34 +0100

    Add lint:report to build process

--- cli/build.sh
+++ cli/build.sh
@@ -5,22 +5,23 @@ cd ..
 
 echo '--> Install dependencies...'
 npm i
-echo "Install done..."
+echo "Done..."
 
 echo '--> Run Build...'
 npm run CRA:build
-echo "Build done..."
+echo "Done..."
 
-echo '--> Run Analysis...'
+echo '--> Run Analysis and Lint Report...'
 npm run analyze
-echo "Analysis done..."
+npm run lint:report
+echo "Done..."
 
 echo '--> Run Coverage...'
 ## Need to push coverato due to this issue https://github.com/facebook/jest/issues/9324
 set -e
 ! npm run test:coverage
 if [ $? -eq 0 ]; then
-  echo "Coverage done..."
+  echo "Done..."
 else
   echo "Always the same error..."
 fi
