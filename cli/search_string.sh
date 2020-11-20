828781fe868a6e9c85612f44c60b0e745b28bbb3
Paolo Cattani <paolocattani0501@gmail.com>
2020-11-20 09:50:42 +0100

    Update search_string.sh

--- cli/search_string.sh
+++ cli/search_string.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
+
 echo "Searching for $SEARCH_STRING in $SEARCH_FILE"
-SEARCH_RESULT=$(awk -v SEARCH_STRING="$SEARCH_STRING" '$SEARCH_STRING \
-    {
-        p=index($1,"=");
-        print substr($1,p+1) ;
-    }' "$SEARCH_FILE")
-echo "Search result : $SEARCH_RESULT"
+SEARCH_RESULT=$(python $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/search_string.py $SEARCH_STRING $SEARCH_FILE 2>&1 >/dev/null)
+echo "Search result : "$SEARCH_RESULT
