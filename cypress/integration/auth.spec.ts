79aa9bf1e21d9b790351d020da6d0b0a39d9d692
Paolo Cattani <paolocattani0501@gmail.com>
2020-11-23 18:17:51 +0100

    Just require json

--- cypress/integration/auth.spec.ts
+++ cypress/integration/auth.spec.ts
@@ -1,7 +1,10 @@
 // enables intelligent code completion for Cypress commands
 // https://on.cypress.io/intelligent-code-completion
 
+
 import {LandingPage, RegistrationProps} from "../pages/landing.page";
+
+const {users:{admin,user}} = require('../fixtures/auth.fixture.json')
 
 describe('Authentication Test', () => {
 	before(() => {
@@ -9,14 +12,12 @@ describe('Authentication Test', () => {
 	});
 
 	beforeEach(function() {
-    cy.fixture('auth.fixture','utf-8').as('authFixture');
 		//Cypress.Cookies.preserveOnce('session_id')
   });
 
   describe('Registration process', function () {
 
-		it('Should register Admin', function () {
-			const admin = this.authFixture.users.admin as RegistrationProps;
+		it('Should register Admin', ()=> {
 			const landingPage = new LandingPage();
 			landingPage.forceDeleteUser(admin.email,admin.username,admin.password);
 			// https://glebbahmutov.com/blog/keep-passwords-secret-in-e2e-tests/
@@ -25,17 +26,15 @@ describe('Authentication Test', () => {
 			cy.getCookies()
 				.should('have.length', 1)
 				.then((cookies) => {
-					expect(cookies[0]).to.have.property('name', 'session_id')
-					//expect(cookies[0]).to.have.property('value', '123ABC')
-					//expect(cookies[0]).to.have.property('domain')
-					expect(cookies[0]).to.have.property('httpOnly')
-					expect(cookies[0]).to.have.property('path')
-					expect(cookies[0]).to.have.property('secure')
+					expect(cookies[0]).to.have.property('name', 'session_id');
+					expect(cookies[0]).to.have.property('domain','localhost');
+					expect(cookies[0]).to.have.property('httpOnly');
+					expect(cookies[0]).to.have.property('path');
+					expect(cookies[0]).to.have.property('secure',false);
 				})
 		});
 
-    it('Should register User', function () {
-			const user = this.authFixture.users.user as RegistrationProps;
+    it('Should register User', ()=> {
 			const landingPage = new LandingPage();
 			landingPage.forceDeleteUser(user.email,user.username,user.password);
 			landingPage.register(user,false);
@@ -43,17 +42,15 @@ describe('Authentication Test', () => {
     });
   });
 
-  describe('Login process', function () {
-    it('Should login as Admin', function () {
-        const admin = this.authFixture.users.admin as RegistrationProps;
+  describe('Login process', ()=> {
+    it('Should login as Admin', ()=> {
 				const landingPage = new LandingPage();
 				landingPage.login(admin);
 				landingPage.getHeaderUsername().should('be.visible').and('contain',admin.username);
 				landingPage.forceLogout();
     });
 
-    it('Should login as User', function () {
-        const user = this.authFixture.users.user as RegistrationProps;
+    it('Should login as User', ()=> {
 				const landingPage = new LandingPage();
 				landingPage.login(user);
 				landingPage.getHeaderUsername().should('be.visible').and('contain',user.username);
