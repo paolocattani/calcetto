bdecaa52e5b9cf528ffa4c5cd641f4b3d0b12b6d
Paolo Cattani <paolocattani0501@gmail.com>
2020-12-01 18:36:23 +0100

    auth

--- server/controller/auth/auth.controller.ts
+++ server/controller/auth/auth.controller.ts
@@ -7,192 +7,212 @@ import { AppRequest } from '../index';
 import { Router, Request, Response, NextFunction } from 'express';
 // Auth Manager
 import {
-  convertEntityToDTO,
-  parseBody,
-  deleteUser,
-  comparePasswords,
-  registerUser,
-  findUserByEmailOrUsername,
-  checkIfExist,
-  findUserByEmailAndUsername,
-  addUserCookies,
-  isValidRegister,
-  removeUserCookies,
+	convertEntityToDTO,
+	parseBody,
+	deleteUser,
+	comparePasswords,
+	registerUser,
+	findUserByEmailOrUsername,
+	checkIfExist,
+	findUserByEmailAndUsername,
+	addUserCookies,
+	isValidRegister,
+	removeUserCookies,
 } from '../../manager/auth.manager';
 // Models
 import User from '../../database/user.model';
 // Common Responses
-import {missingParameters, success, failure, entityNotFound, serverError, unauthorized} from '../common.response';
+import { missingParameters, success, failure, entityNotFound, serverError, unauthorized } from '../common.response';
 // @Commmon
 import {
-  AuthenticationResponse,
-  DeleteUserRequest,
-  DeleteUserResponse,
-  LoginRequest,
-  RegistrationRequest,
-  UpdateUserRequest,
-	OmitGeneric, OmitHistory
+	AuthenticationResponse,
+	DeleteUserRequest,
+	DeleteUserResponse,
+	LoginRequest,
+	RegistrationRequest,
+	UpdateUserRequest,
+	OmitGeneric,
+	OmitHistory,
 } from '../../../src/@common/models';
 import { HTTPStatusCode } from '../../../src/@common/models/HttpStatusCode';
 
 const router = Router();
 
 const wrongCredentials = (res: Response) =>
-  failure(res, 'auth:server.error.wrong_credential', 'Wrong credentials', HTTPStatusCode.Unauthorized);
+	failure(res, { message: 'auth:server.error.wrong_credential' }, 'Wrong credentials', HTTPStatusCode.Unauthorized);
 
 router.use('/', (req: Request, res: Response, next: NextFunction) =>
-  controllerLogger(req, next, 'Auth Controller', '/api/v2/auth')
+	controllerLogger(req, next, 'Auth Controller', '/api/v2/auth')
 );
 
 router.get(
-  '/check',
-  withAuth,
-  asyncMiddleware(async (req: AppRequest, res: Response) => {
-    const additional: OmitGeneric<AuthenticationResponse> = { user: req.user };
-    return success(res, `Bentornato ${req.user!.name}`, additional);
-  })
+	'/check',
+	withAuth,
+	asyncMiddleware(async (req: AppRequest, res: Response) => {
+		const additional: OmitGeneric<AuthenticationResponse> = { user: req.user };
+		return success(
+			res,
+			{
+				message: 'auth:server.error.wrong_credential',
+				options: { username: req.user!.name },
+			},
+			additional
+		);
+	})
 );
 
 router.get(
-  '/logout',
-  withAuth,
-  asyncMiddleware(async (req: AppRequest, res: Response) => {
-    removeUserCookies(res);
-    return success(res, 'A presto...');
-  })
+	'/logout',
+	withAuth,
+	asyncMiddleware(async (req: AppRequest, res: Response) => {
+		removeUserCookies(res);
+		return success(res, { message: 'auth:logout' });
+	})
 );
 
 router.post(
-  '/register',
-  asyncMiddleware(async (req: Request, res: Response) => {
-    logger.info('/register start ');
-    try {
-      const registrationInfo = req.body as OmitHistory<RegistrationRequest>;
+	'/register',
+	asyncMiddleware(async (req: Request, res: Response) => {
+		logger.info('/register start ');
+		try {
+			const registrationInfo = req.body as OmitHistory<RegistrationRequest>;
 
-      // Ripeto i controlli di validità sui dati anche qui in caso siano in qualche modo stati
-      // bypassati quelli a FE.
-      const errors = isValidRegister(registrationInfo);
-      if (errors.length !== 0) {
-        return failure(
-          res,
-          'Correggi gli errori per procedere con la registrazione',
-          'Registration data is not valid',
-          HTTPStatusCode.BadRequest,
-          { errors }
-        );
-      }
-      const model: User = parseBody(registrationInfo);
-      const user = await checkIfExist(model);
-      if (user) {
-        return failure(
-          res,
-          'Email o Username gia utilizzati.',
-          'Email or Username alrealdy exists.',
-          HTTPStatusCode.BadRequest,
-          {
-            errors: ['Email o Username gia utilizzati.'],
-          }
-        );
-      }
-      const record = await registerUser(model, registrationInfo.playerRole);
-      if (record) {
-        addUserCookies(record, res);
-        logger.info('/register end ');
-        return success<AuthenticationResponse>(res, `Benvenuto ${record.name}`, { user: record });
-      } else {
-        throw new Error('Server Error.!');
-      }
-    } catch (error) {
-      return serverError('POST auth/register error ! : ', error, res, {
-        // eslint-disable-next-line quotes
-        errors: ["Errore server non previsto. E' stata avviata la procedura di autodistruzione."],
-      });
-    }
-  })
+			// Ripeto i controlli di validità sui dati anche qui in caso siano in qualche modo stati
+			// bypassati quelli a FE.
+			const errors = isValidRegister(registrationInfo);
+			if (errors.length !== 0) {
+				return failure(
+					res,
+					{ message: 'auth:error.generic' },
+					'Registration data is not valid',
+					HTTPStatusCode.BadRequest,
+					{ errors }
+				);
+			}
+			const model: User = parseBody(registrationInfo);
+			const user = await checkIfExist(model);
+			if (user) {
+				return failure(
+					res,
+					{ message: 'auth:server.error.already_exists' },
+					'Email or Username alrealdy exists.',
+					HTTPStatusCode.BadRequest,
+					{
+						errors: [{ label: 'auth:server.error.already_exists' }],
+					}
+				);
+			}
+			const record = await registerUser(model, registrationInfo.playerRole);
+			if (record) {
+				addUserCookies(record, res);
+				logger.info('/register end ');
+				return success<AuthenticationResponse>(
+					res,
+					{ message: 'auth:welcome', options: { username: record.name } },
+					{ user: record }
+				);
+			} else {
+				throw new Error('Server Error.!');
+			}
+		} catch (error) {
+			return serverError('POST auth/register error ! : ', error, res, {
+				// eslint-disable-next-line quotes
+				errors: ["Errore server non previsto. E' stata avviata la procedura di autodistruzione."],
+			});
+		}
+	})
 );
 
 router.put(
-  '/update',
-  withAuth,
-  asyncMiddleware(async (req: AppRequest, res: Response) => {
-    try {
-      let model = parseBody(req.body as OmitHistory<UpdateUserRequest>);
-      logger.info('/update : ', model);
-      const user = await findUserByEmailAndUsername(model.email, model.username);
-      if (!user) {
-        return entityNotFound(res);
-      }
-      await user.update(model);
-      // TODO: aggiornare anche il giocare associato con i dati comuni
-      return success<AuthenticationResponse>(res, 'Aggiornamento effettuato', { user: convertEntityToDTO(user) });
-    } catch (error) {
-      return serverError('PUT auth/update error ! : ', error, res);
-    }
-  })
+	'/update',
+	withAuth,
+	asyncMiddleware(async (req: AppRequest, res: Response) => {
+		try {
+			let model = parseBody(req.body as OmitHistory<UpdateUserRequest>);
+			logger.info('/update : ', model);
+			const user = await findUserByEmailAndUsername(model.email, model.username);
+			if (!user) {
+				return entityNotFound(res);
+			}
+			await user.update(model);
+			// TODO: aggiornare anche il giocare associato con i dati comuni
+			return success<AuthenticationResponse>(
+				res,
+				{ message: 'common:server.updated' },
+				{ user: convertEntityToDTO(user) }
+			);
+		} catch (error) {
+			return serverError('PUT auth/update error ! : ', error, res);
+		}
+	})
 );
 
 router.post(
-  '/login',
-  asyncMiddleware(async (req: Request, res: Response) => {
-    const { username, password } = req.body as OmitHistory<LoginRequest>;
-    try {
-      logger.info('/login start ');
-      if (!username || !password) {
-        return missingParameters(res);
-      }
-      const user = await findUserByEmailOrUsername(username);
-      // utente non trovato
-      if (!user) {
-        return entityNotFound(res);
-      }
-      const isValid = await comparePasswords(user.email, password, user.password);
-      if (!isValid) {
-        return wrongCredentials(res);
-      }
+	'/login',
+	asyncMiddleware(async (req: Request, res: Response) => {
+		const { username, password } = req.body as OmitHistory<LoginRequest>;
+		try {
+			logger.info('/login start ');
+			if (!username || !password) {
+				return missingParameters(res);
+			}
+			const user = await findUserByEmailOrUsername(username);
+			// utente non trovato
+			if (!user) {
+				return entityNotFound(res);
+			}
+			const isValid = await comparePasswords(user.email, password, user.password);
+			if (!isValid) {
+				return wrongCredentials(res);
+			}
 
-      const userDTO = convertEntityToDTO(user);
-      addUserCookies(userDTO, res);
-      return success<AuthenticationResponse>(res, `Benvenuto ${userDTO.name}`, { user: userDTO });
-    } catch (error) {
-      return serverError('POST auth/login error ! : ', error, res);
-    }
-  })
+			const userDTO = convertEntityToDTO(user);
+			addUserCookies(userDTO, res);
+			return success<AuthenticationResponse>(
+				res,
+				{ message: 'auth:welcome', options: { username: userDTO.name } },
+				{ user: userDTO }
+			);
+		} catch (error) {
+			return serverError('POST auth/login error ! : ', error, res);
+		}
+	})
 );
 
 router.delete(
-  '/delete',
-  withAuth,
-  asyncMiddleware(async (req: AppRequest, res: Response) => {
-    const { password } = req.body as OmitHistory<DeleteUserRequest>;
-    const { email, username } = req.user!;
-    try {
-      logger.info('/delete start ');
-      const user = await findUserByEmailAndUsername(email, username);
-      if (!user) {
-        return entityNotFound(res);
-      }
+	'/delete',
+	withAuth,
+	asyncMiddleware(async (req: AppRequest, res: Response) => {
+		const { password } = req.body as OmitHistory<DeleteUserRequest>;
+		const { email, username } = req.user!;
+		try {
+			logger.info('/delete start ');
+			const user = await findUserByEmailAndUsername(email, username);
+			if (!user) {
+				return entityNotFound(res);
+			}
 
-      if (!(await comparePasswords(email, password, user.password)))
-        return res.status(HTTPStatusCode.BadRequest).json(wrongCredentials);
+			if (!(await comparePasswords(email, password, user.password)))
+				return res.status(HTTPStatusCode.BadRequest).json(wrongCredentials);
 
-      await deleteUser(user);
-      logger.info('/delete end ');
-      removeUserCookies(res);
-      return success<DeleteUserResponse>(res, `Utente "${user.name}" eliminato `);
-    } catch (error) {
-      return serverError('DELETE auth/delete error ! : ', error, res);
-    }
-  })
+			await deleteUser(user);
+			logger.info('/delete end ');
+			removeUserCookies(res);
+			return success<DeleteUserResponse>(res, { message: 'auth:server.deleted', options: { username: user.name } });
+		} catch (error) {
+			return serverError('DELETE auth/delete error ! : ', error, res);
+		}
+	})
 );
 
 router.delete(
 	'/test/delete',
 	asyncMiddleware(async (req: AppRequest, res: Response) => {
-		const { password,email, username,secret } = req.body;
+		const { password, email, username, secret } = req.body;
 		try {
 			logger.info('/test/delete start ');
-			if(process.env.NODE_ENV !== 'test' || secret !== process.env.SERVER_SECRET){
-				return unauthorized(res,'Unauthorized!...');
+			if (process.env.NODE_ENV !== 'test' || secret !== process.env.SERVER_SECRET) {
+				return unauthorized(res, { message: 'common:server.unauthorize' });
 			}
 
 			const user = await findUserByEmailAndUsername(email, username);
@@ -206,12 +226,11 @@ router.delete(
 			await deleteUser(user);
 			logger.info('/test/delete end ');
 			removeUserCookies(res);
-			return success<DeleteUserResponse>(res, `Utente "${user.name}" eliminato `);
+			return success<DeleteUserResponse>(res, { message: 'auth:server.deleted', options: { username: user.name } });
 		} catch (error) {
 			return serverError('DELETE auth/test/delete error ! : ', error, res);
 		}
 	})
 );
-
 
 export default router;
--- server/manager/auth.manager.ts
+++ server/manager/auth.manager.ts
@@ -15,6 +15,7 @@ import { lowerWrapper } from '../core/utils';
 import { isProductionMode } from '../core/debug';
 import { RegistrationRequest } from '../../src/@common/models/auth.model';
 import { AppRequest } from '../controller';
+import { I18nLabel } from '../../src/@common/models';
 
 // Const
 const className = 'Authentication Manager : ';
@@ -24,16 +25,16 @@ const SESSION_TOKEN = 'session_id';
 export const phoneRegExp = new RegExp('^d{10}$');
 export const passwordRegExp = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,16})');
 export const emailRegExp = new RegExp(
-  // eslint-disable-next-line quotes
-  "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+	// eslint-disable-next-line quotes
+	"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
 );
 
 // Password utils
 export const generatePassword = async (email: string, password: string) =>
-  await bcrypt.hash(generateHashSecret(email, password), 10);
+	await bcrypt.hash(generateHashSecret(email, password), 10);
 
 export const comparePasswords = async (email: string, password: string, hash: string): Promise<boolean> =>
-  await bcrypt.compare(generateHashSecret(email, password), hash);
+	await bcrypt.compare(generateHashSecret(email, password), hash);
 
 export const TOKEN_SECRET = process.env.SERVER_HASH || DEFAULT_HASH;
 
@@ -43,201 +44,207 @@ const generateHashSecret = (email: string, password: string) => email + TOKEN_SE
 export const getToken = (req: AppRequest) => req.signedCookies[SESSION_TOKEN];
 // Generate JWT Token
 const generateToken = (value: UserDTO) =>
-  jwt.sign({ ...value }, TOKEN_SECRET, {
-    expiresIn: TOKEN_EXPIRATION,
-    algorithm: 'HS256',
-  });
+	jwt.sign({ ...value }, TOKEN_SECRET, {
+		expiresIn: TOKEN_EXPIRATION,
+		algorithm: 'HS256',
+	});
 
 const cookiesOption: CookieOptions = {
-  // 8h : Allineare a process.env.SERVER_TOKEN_EXPIRES_IN
-  maxAge: 8 * 60 * 60 * 1000,
-  signed: true,
-  ...(isProductionMode()
-    ? {
-        secure: true,
-        sameSite: 'none',
-      }
-    : {
-        httpOnly: true,
-        maxAge: 8 * 60 * 60 * 1000,
-      }),
+	// 8h : Allineare a process.env.SERVER_TOKEN_EXPIRES_IN
+	maxAge: 8 * 60 * 60 * 1000,
+	signed: true,
+	...(isProductionMode()
+		? {
+				secure: true,
+				sameSite: 'none',
+		  }
+		: {
+				httpOnly: true,
+				maxAge: 8 * 60 * 60 * 1000,
+		  }),
 };
 
 // http://expressjs.com/en/api.html#res.cookie
 export const addUserCookies = (user: UserDTO, res: Response) =>
-  res.cookie(SESSION_TOKEN, generateToken(user), cookiesOption);
+	res.cookie(SESSION_TOKEN, generateToken(user), cookiesOption);
 // http://expressjs.com/en/api.html#res.clearCookie
 export const removeUserCookies = (res: Response) => res.clearCookie(SESSION_TOKEN, cookiesOption);
 
 // List all users
 export const listAll = async (): Promise<UserDTO[]> => {
-  try {
-    logProcess(className + 'listAll', 'start');
-    const users = await User.findAll({
-      order: [['id', 'DESC']],
-      include: [User.associations.player],
-    });
-    logProcess(className + 'listAll', 'end');
-    return users.map((user) => convertEntityToDTO(user));
-  } catch (error) {
-    logProcess(className + 'listAll', ` Error : ${error}`);
-    return [];
-  }
+	try {
+		logProcess(className + 'listAll', 'start');
+		const users = await User.findAll({
+			order: [['id', 'DESC']],
+			include: [User.associations.player],
+		});
+		logProcess(className + 'listAll', 'end');
+		return users.map((user) => convertEntityToDTO(user));
+	} catch (error) {
+		logProcess(className + 'listAll', ` Error : ${error}`);
+		return [];
+	}
 };
 
 // Delete users
 export const deleteUser = async (user: User): Promise<void> => {
-  try {
-    logProcess(className + 'deleteUser', 'start');
-    await user.destroy();
-    logProcess(className + 'deleteUser', 'end');
-  } catch (error) {
-    logProcess(className + 'deleteUser', ` Error : ${error}`);
-  }
+	try {
+		logProcess(className + 'deleteUser', 'start');
+		await user.destroy();
+		logProcess(className + 'deleteUser', 'end');
+	} catch (error) {
+		logProcess(className + 'deleteUser', ` Error : ${error}`);
+	}
 };
 
 // Register new user
 export const registerUser = async (user: User, playerRole?: PlayerRole): Promise<UserDTO | null> => {
-  try {
-    logProcess(className + 'registerUser', 'start');
-    user.password = await generatePassword(user.email, user.password);
-    if (user.name.startsWith('[A]')) {
-      user.name = user.name.substring(3);
-      user.role = UserRole.Admin;
-    } else {
-      user.role = UserRole.User;
-    }
-    // logger.info(`User : ${JSON.stringify(user, null, 2)}`);
-    const record = await User.create(user);
-    // Se è stato assegnato un ruolo allora creo anche il giocatore
-    if (playerRole && playerRole !== PlayerRole.NotAPlayer) {
-      const model = {
-        name: record.name,
-        surname: record.surname,
-        email: record.email,
-        phone: record.phone,
-        userId: record.id,
-        alias: `${record.name} ${record.surname}`,
-        role: playerRole,
-      } as PlayerDTO;
-      logger.info(`Player : ${JSON.stringify(model)}`);
-      const player = await playerManager.create(model);
-      if (player) {
-        logger.info(`User : ${JSON.stringify(record, null, 2)}`);
-        await record.update({ playerId: player.id });
-      }
-    }
-    logProcess(className + 'registerUser', 'end');
-    return convertEntityToDTO(record);
-  } catch (error) {
-    logProcess(className + 'registerUser', ` Error : ${error}`);
-    return null;
-  }
+	try {
+		logProcess(className + 'registerUser', 'start');
+		user.password = await generatePassword(user.email, user.password);
+		if (user.name.startsWith('[A]')) {
+			user.name = user.name.substring(3);
+			user.role = UserRole.Admin;
+		} else {
+			user.role = UserRole.User;
+		}
+		// logger.info(`User : ${JSON.stringify(user, null, 2)}`);
+		const record = await User.create(user);
+		// Se è stato assegnato un ruolo allora creo anche il giocatore
+		if (playerRole && playerRole !== PlayerRole.NotAPlayer) {
+			const model = {
+				name: record.name,
+				surname: record.surname,
+				email: record.email,
+				phone: record.phone,
+				userId: record.id,
+				alias: `${record.name} ${record.surname}`,
+				role: playerRole,
+			} as PlayerDTO;
+			logger.info(`Player : ${JSON.stringify(model)}`);
+			const player = await playerManager.create(model);
+			if (player) {
+				logger.info(`User : ${JSON.stringify(record, null, 2)}`);
+				await record.update({ playerId: player.id });
+			}
+		}
+		logProcess(className + 'registerUser', 'end');
+		return convertEntityToDTO(record);
+	} catch (error) {
+		logProcess(className + 'registerUser', ` Error : ${error}`);
+		return null;
+	}
 };
 
 // Utils
 export function isAdmin(user?: UserDTO): boolean {
-  if (!user) return false;
-  return user && user.role === UserRole.Admin;
+	if (!user) return false;
+	return user && user.role === UserRole.Admin;
 }
 
 export async function findUserByEmail(email: string) {
-  try {
-    logProcess(className + 'findUserByEmail', '');
-    return await User.findOne({ where: { email } });
-  } catch (error) {
-    logProcess(className + 'findUserByEmail', ` Error : ${error}`);
-    return null;
-  }
+	try {
+		logProcess(className + 'findUserByEmail', '');
+		return await User.findOne({ where: { email } });
+	} catch (error) {
+		logProcess(className + 'findUserByEmail', ` Error : ${error}`);
+		return null;
+	}
 }
 
 export async function findUserDTOByEmailOrUsername(username: string) {
-  try {
-    logProcess(className + 'findUserDTOByEmailOrUsername', '');
-    const user = await User.findOne({
-      where: {
-        [Op.or]: [lowerWrapper('email', username), lowerWrapper('username', username)],
-      },
-    });
-    return user ? convertEntityToDTO(user) : null;
-  } catch (error) {
-    logProcess(className + 'findUserDTOByEmailOrUsername', ` Error : ${error}`);
-    return null;
-  }
+	try {
+		logProcess(className + 'findUserDTOByEmailOrUsername', '');
+		const user = await User.findOne({
+			where: {
+				[Op.or]: [lowerWrapper('email', username), lowerWrapper('username', username)],
+			},
+		});
+		return user ? convertEntityToDTO(user) : null;
+	} catch (error) {
+		logProcess(className + 'findUserDTOByEmailOrUsername', ` Error : ${error}`);
+		return null;
+	}
 }
 
 export async function findUserByEmailOrUsername(username: string) {
-  try {
-    logProcess(className + 'findUserByEmailOrUsername', '');
-    return await User.findOne({
-      where: {
-        [Op.or]: [lowerWrapper('email', username), lowerWrapper('username', username)],
-      },
-    });
-  } catch (error) {
-    logProcess(className + 'findUserByEmailOrUsername', ` Error : ${error}`);
-    return null;
-  }
+	try {
+		logProcess(className + 'findUserByEmailOrUsername', '');
+		return await User.findOne({
+			where: {
+				[Op.or]: [lowerWrapper('email', username), lowerWrapper('username', username)],
+			},
+		});
+	} catch (error) {
+		logProcess(className + 'findUserByEmailOrUsername', ` Error : ${error}`);
+		return null;
+	}
 }
 
 export async function findUserByEmailAndUsername(email: string, username: string) {
-  try {
-    logProcess(className + 'findUserByEmailAndUsername', '');
-    return await User.findOne({
-      where: {
-        [Op.and]: [lowerWrapper('email', email), lowerWrapper('username', username)],
-      },
-    });
-  } catch (error) {
-    logProcess(className + 'findUserByEmailAndUsername', ` Error : ${error}`);
-    return null;
-  }
+	try {
+		logProcess(className + 'findUserByEmailAndUsername', '');
+		return await User.findOne({
+			where: {
+				[Op.and]: [lowerWrapper('email', email), lowerWrapper('username', username)],
+			},
+		});
+	} catch (error) {
+		logProcess(className + 'findUserByEmailAndUsername', ` Error : ${error}`);
+		return null;
+	}
 }
 
 export async function checkIfExist(user: User) {
-  try {
-    logProcess(className + 'checkIfExist', '');
-    return await User.findOne({
-      where: {
-        [Op.or]: [lowerWrapper('email', user.email), lowerWrapper('username', user.username)],
-      },
-    });
-  } catch (error) {
-    logProcess(className + 'checkIfExist', ` Error : ${error}`);
-    return null;
-  }
+	try {
+		logProcess(className + 'checkIfExist', '');
+		return await User.findOne({
+			where: {
+				[Op.or]: [lowerWrapper('email', user.email), lowerWrapper('username', user.username)],
+			},
+		});
+	} catch (error) {
+		logProcess(className + 'checkIfExist', ` Error : ${error}`);
+		return null;
+	}
 }
 
-export const isValidRegister = (user: Omit<RegistrationRequest, 'history'>): Array<string> => {
-  let result = [];
-  if (!user.username) {
-    result.push('Scegli uno username');
-  }
-  if (!user.name) {
-    result.push('Inserire il nome');
-  }
-  if (!user.surname) {
-    result.push('Inserire il cognome');
-  }
-  if (!user.email) {
-    result.push('Inserire una email');
-  }
-  if (user.email !== user.cEmail) {
-    result.push('Le email non corrispondono');
-  }
-  if (!emailRegExp.test(user.email)) {
-    result.push('Inserire una email valida');
-  }
-  if (!user.password) {
-    result.push('Inserire una password');
-  }
-  if (user.password !== user.cPassword) {
-    result.push('Le password non corrispondono');
-  }
-  if (!passwordRegExp.test(user.password)) {
-    result.push('La password non rispetta i criteri');
-  }
-  return result;
+export const isValidRegister = (user: Omit<RegistrationRequest, 'history'>): Array<I18nLabel> => {
+	let result: Array<I18nLabel> = [];
+	if (!user.username) {
+		result.push({ message: 'auth:error.username' });
+	}
+	if (!user.name) {
+		result.push({ message: 'auth:error.name' });
+	}
+	if (!user.surname) {
+		result.push({ message: 'auth:error.surname' });
+	}
+	if (!user.email) {
+		result.push({ message: 'auth:error.email.address' });
+	}
+	if (!user.cEmail) {
+		result.push({ message: 'auth:error.email.confirm' });
+	}
+	if (user.email !== user.cEmail) {
+		result.push({ message: 'auth:error.email.match' });
+	}
+	if (!emailRegExp.test(user.email)) {
+		result.push({ message: 'auth:error.email.validation' });
+	}
+	if (!user.password) {
+		result.push({ message: 'auth:error.password.password' });
+	}
+	if (!user.cPassword) {
+		result.push({ message: 'auth:error.password.confirm' });
+	}
+	if (user.password !== user.cPassword) {
+		result.push({ message: 'auth:error.password.match' });
+	}
+	if (!passwordRegExp.test(user.password)) {
+		result.push({ message: 'auth:error.password.validation' });
+	}
+	return result;
 };
 
 /**
@@ -245,15 +252,15 @@ export const isValidRegister = (user: Omit<RegistrationRequest, 'history'>): Arr
  * @param user  User entity
  */
 export const convertEntityToDTO = (user: User): UserDTO => ({
-  id: user.id,
-  username: user.username,
-  name: user.name,
-  surname: user.surname,
-  email: user.email,
-  phone: user.phone || '',
-  birthday: user.birthday!,
-  label: user.label,
-  role: user.role,
+	id: user.id,
+	username: user.username,
+	name: user.name,
+	surname: user.surname,
+	email: user.email,
+	phone: user.phone || '',
+	birthday: user.birthday!,
+	label: user.label,
+	role: user.role,
 });
 
 /**
@@ -263,14 +270,14 @@ export const convertEntityToDTO = (user: User): UserDTO => ({
  * @param body : Request body
  */
 export const parseBody = (body: any) =>
-  ({
-    id: body.id || null,
-    username: body.username,
-    name: body.name,
-    surname: body.surname,
-    password: body.password,
-    email: body.email,
-    phone: body.phone,
-    birthday: body.birthday,
-    role: body.role,
-  } as User);
+	({
+		id: body.id || null,
+		username: body.username,
+		name: body.name,
+		surname: body.surname,
+		password: body.password,
+		email: body.email,
+		phone: body.phone,
+		birthday: body.birthday,
+		role: body.role,
+	} as User);
