8ba44f2529185cd09c574a5f097671a0af164c38
Paolo Cattani <paolocattani0501@gmail.com>
2020-12-09 09:29:08 +0100

    Update coverage from cypress

--- cypress/integration/tournament.e2e.ts
+++ cypress/integration/tournament.e2e.ts
@@ -1,18 +1,34 @@
-
-
-
+import {Tournament} from "../pages/tournament.page";
 
 
 describe('Tournament Test', () => {
 
 	describe('Login as Admin', () => {
-		beforeEach(()=>{
-
+		let tournamentPage = new Tournament();
+		before(()=>{
+			tournamentPage.forceRegisterAdmin();
+			Cypress.Cookies.preserveOnce('session_id')
 		})
+		it('Should register Admin', ()=> {
+			tournamentPage.visit();
+			tournamentPage.getForm().should('be.visible');
+			tournamentPage.getNewTournamentButton().should('be.visible');
+		});
 
 	});
 
 	describe('Login as User', () => {
+
+		let tournamentPage = new Tournament();
+		before(()=>{
+			tournamentPage.forceRegisterUser();
+			Cypress.Cookies.preserveOnce('session_id')
+		})
+		it('Should register Admin', ()=> {
+			tournamentPage.visit();
+			tournamentPage.getForm().should('be.visible');
+			tournamentPage.getNewTournamentButton().should('be.visible');
+		});
 
 	});
 
