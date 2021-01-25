53adcd0037b7cdcad8d8368e210728f08855e1d6
Paolo Cattani <paolocattani0501@gmail.com>
2021-01-25 16:56:56 +0100

    Update stat.tsx

--- src/components/Pair/stat.tsx
+++ src/components/Pair/stat.tsx
@@ -1,31 +1,7 @@
 import React, { CSSProperties } from 'react';
-import { Badge, Col, Row, Table } from 'react-bootstrap';
-import { Variant } from 'react-bootstrap/esm/types';
+import { Table } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { StatsPairDTO } from 'src/@common/dto/stats/stats.pairs.dto';
-
-interface StatsBadgeProps {
-	header: string;
-	variant: Variant;
-	stats: Array<number>;
-}
-const StatsBadge: React.FC<StatsBadgeProps> = ({ stats, variant, header }) => (
-	<Col>
-		<Row key={header + '0'} className="justify-content-center">
-			<strong>{header}</strong>
-		</Row>
-		<hr />
-		{stats.map((s, i) => (
-			<Row key={header + i} className="justify-content-center">
-				<Col>
-					<Badge pill variant={variant}>
-						{s}
-					</Badge>
-				</Col>
-			</Row>
-		))}
-	</Col>
-);
 
 interface StatsPairProps {
 	stats: StatsPairDTO;
@@ -36,29 +12,7 @@ const StatsPair: React.FC<StatsPairProps> = ({ stats }) => {
 	const stage2 = t('stats:stage2');
 	const tot = t('stats:tot');
 
-	const winnings = [stats.s1win, stats.s2win, stats.totwin];
-	const defeats = [stats.s1def, stats.s2def, stats.totdef];
-	const winningsPercentage = [stats.ratiotot, stats.ratiotot, stats.ratiotot];
-	const defeatsPercentage = [stats.ratiotot, stats.ratiotot, stats.ratiotot];
-
-	console.log('StatsPair : ', stats);
-	const test1 = (
-		<Row>
-			<Col>
-				<Row className="justify-content-center">
-					<strong></strong>
-				</Row>
-				<hr />
-				<Row>{stage1}</Row>
-				<Row>{stage2}</Row>
-				<Row>{tot}</Row>
-			</Col>
-			<StatsBadge variant="success" stats={winnings} header={t('stats:winnings')} />
-			<StatsBadge variant="danger" stats={defeats} header={t('stats:defeats')} />
-			<StatsBadge variant="success" stats={winningsPercentage} header={t('stats:ratio')} />
-			<StatsBadge variant="danger" stats={defeatsPercentage} header={t('stats:ratio')} />
-		</Row>
-	);
+	//console.log('StatsPair : ', stats);
 	const darkBackGround: CSSProperties = {
 		backgroundColor: 'var(--default-black)',
 		color: 'whitesmoke',
