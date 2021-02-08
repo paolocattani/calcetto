612203ea4b9168cc4039bf8b8233ea6cd05d2975
Paolo Cattani <paolocattani0501@gmail.com>
2021-02-09 00:51:23 +0100

    stats

--- src/components/Tournament/bests.pairs.component.tsx
+++ src/components/Tournament/bests.pairs.component.tsx
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { Badge, Carousel, Col, Row } from 'react-bootstrap';
+import { Carousel, Col, Row } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
-import { StatsPairDTO } from 'src/@common/dto';
-import { StatsBestPairsResponse } from 'src/@common/models';
+import { StatsPairDTO, StatsPlayerDTO } from 'src/@common/dto';
+import { StatsBestPairsResponse, StatsBestPlayersResponse } from 'src/@common/models';
 import { formatDate, roundNumber } from 'src/@common/utils';
-import { fetchBestPairs } from 'src/redux/services/stats.service';
-import { LeftAngleIcon, LeftArrowIcon, RightAngleIcon, RightArrowIcon } from '../core/icons';
+import { fetchBestPairs, fetchBestPlayers } from 'src/redux/services/stats.service';
+import { LeftAngleIcon, RightAngleIcon } from '../core/icons';
 import { getLabel } from './helper';
 
-type StatsType = {
-	ever: Array<StatsPairDTO>;
-	week: Array<StatsPairDTO>;
-	month: Array<StatsPairDTO>;
+type StatsType<T> = {
+	ever: Array<T>;
+	week: Array<T>;
+	month: Array<T>;
 };
 const aWeekAgo = new Date();
 aWeekAgo.setDate(aWeekAgo.getDate() - 7);
@@ -23,14 +23,16 @@ interface TheBestsProps {}
 const TheBests: React.FC<TheBestsProps> = () => {
 	const MAX_RESULT = 4;
 	const { t } = useTranslation(['stats']);
-	const [loaded, setLoaded] = useState<boolean>(false);
-	const [stats, setStats] = useState<StatsType>();
+	const [loadedPair, setLoadedPair] = useState<boolean>(false);
+	const [statsPair, setStatsPair] = useState<StatsType<StatsPairDTO>>();
+	const [loadedPlayer, setLoadedPlayer] = useState<boolean>(false);
+	const [statsPlayer, setStatsPlayer] = useState<StatsType<StatsPlayerDTO>>();
 	const [index, setIndex] = useState(0);
 	const handleSelect = (selectedIndex: number, event: Record<string, unknown> | null) => {
 		setIndex(selectedIndex);
 	};
 	useEffect(() => {
-		if (!loaded) {
+		if (!loadedPair) {
 			(async () => {
 				try {
 					const { stats: ever } = (await fetchBestPairs({})) as StatsBestPairsResponse;
@@ -38,15 +40,35 @@ const TheBests: React.FC<TheBestsProps> = () => {
 					const { stats: month } = (await fetchBestPairs({
 						from: formatDate(aMonthAgo, '-'),
 					})) as StatsBestPairsResponse;
-					setStats({ ever, week, month });
+					setStatsPair({ ever, week, month });
 				} catch (error) {
 					console.log('Error');
 				} finally {
-					setLoaded(true);
+					setLoadedPair(true);
 				}
 			})();
 		}
-	}, [loaded]);
+	}, [loadedPair]);
+	useEffect(() => {
+		if (!loadedPlayer) {
+			(async () => {
+				try {
+					const { stats: ever } = (await fetchBestPlayers({})) as StatsBestPlayersResponse;
+					const { stats: week } = (await fetchBestPlayers({
+						from: formatDate(aWeekAgo, '-'),
+					})) as StatsBestPlayersResponse;
+					const { stats: month } = (await fetchBestPlayers({
+						from: formatDate(aMonthAgo, '-'),
+					})) as StatsBestPlayersResponse;
+					setStatsPlayer({ ever, week, month });
+				} catch (error) {
+					console.log('Error');
+				} finally {
+					setLoadedPlayer(true);
+				}
+			})();
+		}
+	}, [loadedPlayer]);
 
 	// https://www.w3schools.com/howto/howto_css_user_rating.asp
 	const getPairs = (pairStats: Array<StatsPairDTO>, key: string) =>
@@ -100,7 +122,56 @@ const TheBests: React.FC<TheBestsProps> = () => {
 			</>
 		) : null;
 
-	return stats && Object.keys(stats).length > 0 ? (
+	const getPlayers = (playerStats: Array<StatsPlayerDTO>, key: string) =>
+		playerStats ? (
+			<>
+				<Row>
+					<Col md={{ span: 2, offset: 2 }} style={{ textAlign: 'right' }}>
+						{t('stats:player')}
+					</Col>
+					<Col md="4" style={{ textAlign: 'left' }}>
+						{t('stats:win_percentage')} ( {t(`stats:${key}`)} )
+					</Col>
+				</Row>
+				{playerStats
+					.filter((v, i) => (playerStats.length >= MAX_RESULT ? i < MAX_RESULT : i > -1))
+					.sort(
+						(p1, p2) =>
+							roundNumber((p2.totwin / p2.totMatch) * 100, 1) - roundNumber((p1.totwin / p1.totMatch) * 100, 1)
+					)
+					.map((p, ii) => (
+						<Row>
+							<Col
+								md={{ span: 2, offset: 2 }}
+								style={{ textAlign: 'right', float: 'left', width: '15%', marginTop: '10px', fontSize: 'large' }}
+							>
+								<i>
+									<strong>{getLabel(p.player)}</strong>
+								</i>
+							</Col>
+							<Col md="4" style={{ marginTop: '10px', float: 'left', width: '70%' }}>
+								<div style={{ width: '100%', backgroundColor: '#f1f1f1', textAlign: 'center', color: 'white' }}>
+									<div
+										style={{
+											width: `${roundNumber((p.totwin / p.totMatch) * 100, 1)}%`,
+											height: '18px',
+											backgroundColor: '#ffc107',
+										}}
+									></div>
+								</div>
+							</Col>
+							<Col
+								md={{ span: 2 }}
+								style={{ textAlign: 'left', float: 'left', width: '15%', marginTop: '10px', fontSize: 'large' }}
+							>
+								{roundNumber((p.totwin / p.totMatch) * 100, 1)}%
+							</Col>
+						</Row>
+					))}
+			</>
+		) : null;
+
+	return (statsPair && Object.keys(statsPair).length > 0) || (statsPlayer && Object.keys(statsPlayer).length > 0) ? (
 		<Carousel
 			touch
 			activeIndex={index}
@@ -109,13 +180,24 @@ const TheBests: React.FC<TheBestsProps> = () => {
 			prevIcon={<LeftAngleIcon size="3x" color="#343a40" />}
 			nextIcon={<RightAngleIcon size="3x" color="#343a40" />}
 		>
-			{Object.keys(stats).map((key, ii) =>
-				stats[key as keyof StatsType].length > 0 ? (
-					<Carousel.Item style={{ opacity: 1 }} className="mb-5" key={`${key}-${ii * 18}`} interval={5000}>
-						{getPairs(stats[key as keyof StatsType], key)}
-					</Carousel.Item>
-				) : null
-			)}
+			{statsPair
+				? Object.keys(statsPair).map((key, ii) =>
+						statsPair[key as keyof StatsType<StatsPairDTO>].length > 0 ? (
+							<Carousel.Item style={{ opacity: 1 }} className="mb-5" key={`${key}-${ii * 18}`} interval={5000}>
+								{getPairs(statsPair[key as keyof StatsType<StatsPairDTO>], key)}
+							</Carousel.Item>
+						) : null
+				  )
+				: null}
+			{statsPlayer
+				? Object.keys(statsPlayer).map((key, ii) =>
+						statsPlayer[key as keyof StatsType<StatsPlayerDTO>].length > 0 ? (
+							<Carousel.Item style={{ opacity: 1 }} className="mb-5" key={`${key}-${ii * 18}`} interval={5000}>
+								{getPlayers(statsPlayer[key as keyof StatsType<StatsPlayerDTO>], key)}
+							</Carousel.Item>
+						) : null
+				  )
+				: null}
 		</Carousel>
 	) : null;
 };
