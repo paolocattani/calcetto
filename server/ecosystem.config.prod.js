5d3fa4450299c17a6e03144082a301e8109d5549
Paolo Cattani <paolocattani0501@gmail.com>
2021-03-16 23:13:09 +0100

    Update ecosystem.config.prod.js

--- server/ecosystem.config.prod.js
+++ server/ecosystem.config.prod.js
@@ -8,8 +8,7 @@ module.exports = {
 			interpreter: 'node',
 			env: {
 				NODE_ENV: 'production',
-				ORIGIN_WHITE_LIST:
-					'http://localhost:5001;http://localhost:5000;https://calcetto2020stage.herokuapp.com;https://calcetto2020production.herokuapp.com',
+				ORIGIN_WHITE_LIST: 'calcetto2020stage.herokuapp.com;calcetto2020production.herokuapp.com',
 				SERVER_FORCE: false,
 				SERVER_TOKEN_EXPIRES_IN: '8h',
 				STATIC_CONTENTS_CACHE: 10000,
