04bd3a3a65d1279ea0f2bcc94425585cef9c4c1d
Paolo Cattani <paolocattani0501@gmail.com>
2021-03-17 00:28:03 +0100

    paths

--- server/test/common.ts
+++ server/test/common.ts
@@ -1,28 +1,27 @@
 import {
-  AuthenticationResponse,
-  DeleteUserRequest,
-  LoginRequest,
-  RegistrationRequest,
-} from '../../src/@common/models/auth.model';
-import { OmitHistory } from '../../src/@common/models/common.models';
-import { deleteWrapper, postWrapper } from '../../src/@common/utils/fetch.utils';
-import { PlayerRole } from '../../src/@common/dto/player.dto';
+	AuthenticationResponse,
+	DeleteUserRequest,
+	LoginRequest,
+	RegistrationRequest,
+} from '@common/models/auth.model';
+import { OmitHistory } from '@common/models/common.models';
+import { deleteWrapper, postWrapper } from '@common/utils/fetch.utils';
 import AppServer from '../express/AppServer';
 
 export const registerTestUser = async (
-  registrationRequest: OmitHistory<RegistrationRequest>
+	registrationRequest: OmitHistory<RegistrationRequest>
 ): Promise<AuthenticationResponse> =>
-  await postWrapper<RegistrationRequest, AuthenticationResponse>('/api/v2/auth/register', registrationRequest);
+	await postWrapper<RegistrationRequest, AuthenticationResponse>('/api/v2/auth/register', registrationRequest);
 
 export const loginTestUser = async (loginRequest: OmitHistory<LoginRequest>): Promise<AuthenticationResponse> =>
-  await postWrapper<LoginRequest, AuthenticationResponse>('http://localhost:5001/api/v2/auth/login', loginRequest);
+	await postWrapper<LoginRequest, AuthenticationResponse>('http://localhost:5001/api/v2/auth/login', loginRequest);
 
 export const deleteTestUser = async (deleteRequest: OmitHistory<DeleteUserRequest>): Promise<AuthenticationResponse> =>
-  await deleteWrapper<DeleteUserRequest, AuthenticationResponse>('/api/v2/auth/delete', deleteRequest);
+	await deleteWrapper<DeleteUserRequest, AuthenticationResponse>('/api/v2/auth/delete', deleteRequest);
 
 export async function getServer(): Promise<AppServer> {
-  const server = new AppServer();
-  await server.connect();
-  server.start();
-  return server;
+	const server = new AppServer();
+	await server.connect();
+	server.start();
+	return server;
 }
